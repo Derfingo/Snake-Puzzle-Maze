1cea505ab4c494bce2f3e6d3300c94096d59cc4a
Derfingo <69958932+Derfingo@users.noreply.github.com>
2020-08-27 00:26:08 +0300

    Added game menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,29 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public static bool IsPaused = false;
+    public Animator animator;
+
     public GameObject GameOverPanel;
+    public GameObject PauseMenuPanel;
+
+    private static GameObject PauseMenuPanelStatic;
     private static GameObject gameOverPanelStatic;
+    public static bool IsPaused = false;
 
     private void Awake()
     {
-
         if (gameOverPanelStatic == null)
         {
             gameOverPanelStatic = GameOverPanel;
         }
 
-        IsPaused = false;
-    }
-
-    public void OnPlayHandler()
-    {
-        SceneManager.LoadScene(1);
-    }
-       
-    public void OnExitHandler()
-    {
-        Application.Quit();
+        if (PauseMenuPanelStatic == null)
+        {
+            PauseMenuPanelStatic = PauseMenuPanel;
+        }
     }
 
     public static void RestartGame()
@@ -38,6 +35,21 @@ public class MenuManager : MonoBehaviour
     public static void FailGame()
     {
         gameOverPanelStatic.SetActive(true);
+    }
+
+    public void OnPauseGame()
+    {
         IsPaused = true;
+        PauseMenuPanelStatic.SetActive(true);
+        Time.timeScale = 0f;
+        animator.SetBool("IsTransition", true);
+    }
+
+    public void ONResumeGame()
+    {
+        IsPaused = false;
+        PauseMenuPanelStatic.SetActive(false);
+        Time.timeScale = 1f;
+        animator.SetBool("IsTransition", false);
     }
 }
